762893ff7ff97faac38489d32a2b51e89d84a318
Brendan Woodell <brendan.woodell@gmail.com>
2018-08-08 14:21:55 -0700

    functional pixel art thing

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
+  let currentColor = "white"
+  let curclr = document.querySelector(".curclr")
 
-  
   document.querySelector(".tbl").addEventListener('click', function(e) {
-    var div = e.target.closest('div');
+    var div = e.target.closest('div')
     if( div !== null) {
-      console.log(div.id);
+      div.style.backgroundColor= currentColor
     }
   }, false);
+
+  document.querySelector("footer").addEventListener('click', function(e) {
+    var div = e.target.closest('div');
+    if( div !== null) {
+      currentColor = div.style.backgroundColor
+      curclr.style.backgroundColor = currentColor
+    }
+    }, false);
 
 
 
